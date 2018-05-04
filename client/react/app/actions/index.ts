d8aef9876370831bb57fff7632fca55a3e1d11a2
benjaminhaven <graner@gmail.com>
2018-05-04 08:26:19 -0700

    Watch taking long time..

--- client/react/app/actions/index.ts
+++ client/react/app/actions/index.ts
@@ -1,7 +1,5 @@
-import * as actionCreators from './actionCreators';
-import * as actionConstants from './actionConstants';
+import * as actionCreatorsImport from './actionCreators';
+import * as actionConstantsImport from './actionConstants';
 
-export default {
-  actionCreators,
-  actionConstants
-};
+export const actionCreators = actionCreatorsImport;
+export const actionConstants = actionConstantsImport;
